c4b3a40f8b4243bbd1d4276e6cfb212047f47897
Cassie Wallace <cassbwallace@gmail.com>
2026-01-09 17:28:33 -0700

    Add FTP zones to settings

--- WorkoutCave/Settings/Settings.swift
+++ WorkoutCave/Settings/Settings.swift
@@ -8,11 +8,31 @@
 import SwiftUI
 
 struct Settings: View {
+    @State var ftp: Int?
+    
     var body: some View {
         List {
-            Text("Power Zones")
+            Section("Set FTP") {
+                TextField("FTP", value: $ftp, format: .number)
+                    .textFieldStyle(.roundedBorder)
+            }
+            Section("Power Zones") {
+                Text("**Zone 1:** TBA")
+                Text("**Zone 2:** TBA")
+                Text("**Zone 3:** TBA")
+                Text("**Zone 4:** TBA")
+                Text("**Zone 5:** TBA")
+                Text("**Zone 6:** TBA")
+                Text("**Zone 7:** TBA")
+            }
         }
+        .listStyle(.plain)
+        .listRowSeparator(.hidden)
         .navigationTitle("Settings")
         .navigationBarTitleDisplayMode(.large)
     }
+}
+
+#Preview {
+    Settings()
 }
