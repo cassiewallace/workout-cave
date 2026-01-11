8a7686a5f5c498fff76cfd29b31fd624fe9e4aba
Cassie Wallace <cassbwallace@gmail.com>
2026-01-11 10:01:49 -0700

    Add some starter workouts

--- WorkoutCave/Workout/WorkoutCatalog.swift
+++ WorkoutCave/Workout/WorkoutCatalog.swift
@@ -15,13 +15,36 @@ struct WorkoutItem: Identifiable {
 enum WorkoutCatalog {
     static func all() -> [WorkoutItem] {
         [
+            // Zwift workouts
             WorkoutItem(
                 id: "40-20",
                 source: zwift(resource: "40-20")
             ),
+
+            // JSON workouts
             WorkoutItem(
-                id: "steady-state",
-                source: json(resource: "steady-state")
+                id: "steady-state-base",
+                source: json(resource: "steady-state-base")
+            ),
+            WorkoutItem(
+                id: "recovery-spin",
+                source: json(resource: "recovery-spin")
+            ),
+            WorkoutItem(
+                id: "30-30-power-intervals",
+                source: json(resource: "30-30-power-intervals")
+            ),
+            WorkoutItem(
+                id: "90-60-tempo-intervals",
+                source: json(resource: "90-60-tempo-intervals")
+            ),
+            WorkoutItem(
+                id: "progressive-warmup",
+                source: json(resource: "progressive-warmup")
+            ),
+            WorkoutItem(
+                id: "endurance-build",
+                source: json(resource: "endurance-build")
             )
         ]
     }
